a9263389cc962f903c1463d935bea3452e521351
Josh Aragon <josharagon99@gmail.com>
2021-04-27 10:47:09 -0600

    Fix indents and finish testing

--- cypress/integration/burritoDaBuilder.spec.js
+++ cypress/integration/burritoDaBuilder.spec.js
@@ -1,13 +1,13 @@
 describe('Initial Page View', () => {
   beforeEach(() => {
     cy.fixture('burritoMock.json')
-    .then(orders => {
+      .then(orders => {
         cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
-            statusCode: 200,
-            body: orders
+          statusCode: 200,
+          body: orders
         })
-    cy.visit('http://localhost:3000/')
-    })
+        cy.visit('http://localhost:3000/')
+      })
   })
 
   it('Should Title on page load', () => {
@@ -22,42 +22,42 @@ describe('Initial Page View', () => {
     cy.get('section').children().should('have.length', 3)
   })
 
-})  
+})
 
 describe('Placing Orders', () => {
   beforeEach(() => {
     cy.fixture('burritoMock.json')
-    .then(orders => {
+      .then(orders => {
         cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
-            statusCode: 200,
-            body: orders
+          statusCode: 200,
+          body: orders
         })
-    cy.visit('http://localhost:3000/')
-    })
+        cy.visit('http://localhost:3000/')
+      })
   })
 
   it('Should be able to enter a name and pick ingredients', () => {
     cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
-        statusCode:200,
-        body: {name: 'Joseph', ingredients: ['beans, steak']}
+      statusCode: 200,
+      body: { name: 'Joseph', ingredients: ['beans, steak'] }
     })
     cy.get('input').type('Joseph')
-    .get('#beans').click()
-    .get('#steak').click()
-    .get('#submit-button').click()
+      .get('#beans').click()
+      .get('#steak').click()
+      .get('#submit-button').click()
     cy.get('section').children().should('have.length', 4)
   })
 
   describe('Error Handling', () => {
     beforeEach(() => {
       cy.fixture('burritoMock.json')
-      .then(orders => {
+        .then(orders => {
           cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
-              statusCode: 200,
-              body: ''
+            statusCode: 200,
+            body: ''
           })
-      cy.visit('http://localhost:3000/')
-      })
+          cy.visit('http://localhost:3000/')
+        })
     })
     it('Should have appropriate message if error while fetching orders', () => {
       cy.get('h2').contains('Error fetching orders')
@@ -65,30 +65,30 @@ describe('Placing Orders', () => {
 
     it('Should warn customer if they have not filled out all fields', () => {
       cy.get('input').type('yomama')
-      .get('#submit-button').click()
-      .get('h4').contains('Please fill out every input field')
-      .get('input').clear()
-      
+        .get('#submit-button').click()
+        .get('h4').contains('Please fill out every input field')
+        .get('input').clear()
+
       cy.get('#beans').click()
-      .get('#submit-button').click()
-      .get('h4').contains('Please fill out every input field')
+        .get('#submit-button').click()
+        .get('h4').contains('Please fill out every input field')
     })
   })
 
   describe('No Orders', () => {
     beforeEach(() => {
       cy.fixture('emptyMock.json')
-      .then(orders => {
+        .then(orders => {
           cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
-              statusCode: 200,
-              body: orders
+            statusCode: 200,
+            body: orders
           })
-      cy.visit('http://localhost:3000/')
-      })
+          cy.visit('http://localhost:3000/')
+        })
     })
 
     it('should show appropriate message if no current orders exist', () => {
-        cy.get('p').last().contains('No orders yet!')
-  })
+      cy.get('p').last().contains('No orders yet!')
+    })
   })
 })
