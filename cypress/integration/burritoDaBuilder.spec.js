c02988adb6bb077ea3a9d7505b0a0eeef3b3bed3
Josh Aragon <josharagon99@gmail.com>
2021-04-27 10:04:48 -0600

    Pass first test with successful stub

--- cypress/integration/burritoDaBuilder.spec.js
+++ cypress/integration/burritoDaBuilder.spec.js
@@ -2,11 +2,14 @@ describe('Initial Page View', () => {
   beforeEach(() => {
     cy.fixture('burritoMock.json')
     .then(orders => {
-        cy.intercept('GET', 'http://localhost:3003/api/v1/orders', {
+        cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
             statusCode: 200,
             body: orders
         })
     cy.visit('http://localhost:3000/')
     })
+  })
+  it('Should Title on page load', () => {
+    cy.get('h1').contains('Burrito Builder')
   })
 })  
